9707535510b91674abc5df272dad8288463b5726
Denis <denis.moldovanu@creativsoft.md>
2023-10-20 13:54:41 +0300

    set scroll effect on mobile and desktop

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
+import { scroller } from "react-scroll";
 import { Nav, Navbar } from "react-bootstrap";
 function Header() {
     const [active, setActive] = useState(null);
+    const [scrollOffset, setScrollOffset] = useState(-160);
 
     const navlink = [
         {
             id: 1,
-            navTitle: "Home",
+            navTitle: "home",
             navlink: "#home",
             navClicked: false,
         },
         {
             id: 2,
-            navTitle: "About",
+            navTitle: "about",
             navlink: "#about",
             navClicked: false,
         },
         {
             id: 3,
-            navTitle: "Qualification",
+            navTitle: "qualification",
             navlink: "#qualification",
             navClicked: false,
         },
         {
             id: 4,
-            navTitle: "Services",
+            navTitle: "services",
             navlink: "#services",
             navClicked: false,
         },
         {
             id: 5,
-            navTitle: "Portfolio",
+            navTitle: "portfolio",
             navlink: "#portfolio",
             navClicked: false,
         },
@@ -39,6 +41,17 @@ function Header() {
     const handleNavLinkClick = (id) => {
         setActive(id);
     };
+
+    useEffect(() => {
+        // Check the screen width and set the appropriate scroll offset
+        const isMobile = window.innerWidth <= 992;
+        if (isMobile) {
+            setScrollOffset(-250);
+        } else {
+            setScrollOffset(-100);
+        }
+    }, []);
+
     return (
         <Navbar collapseOnSelect expand="lg">
             <Navbar.Brand href="#home">
@@ -51,7 +64,14 @@ function Header() {
                         key={item.id}
                         href={item.navlink}
                         className={item.id === active ? "active" : ""}
-                        onClick={() => handleNavLinkClick(item.id)}
+                        onClick={() => {
+                            scroller.scrollTo(item.navTitle, {
+                                smooth: true,
+                                offset: scrollOffset,
+                                duration: 500,
+                            });
+                            handleNavLinkClick(item.id);
+                        }}
                     >
                         {item.navTitle}
                     </Nav.Link>
